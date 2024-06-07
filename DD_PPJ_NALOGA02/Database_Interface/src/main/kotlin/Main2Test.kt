f97eac0952cd652b06180d21537b322b1937e435
aljosaferi <aljosa.golob1@student.um.si>
2024-06-07 20:37:54 +0200

    [DD-238] Add OkHttpLibrary

--- DD_PPJ_NALOGA02/Database_Interface/src/main/kotlin/Main2Test.kt
+++ DD_PPJ_NALOGA02/Database_Interface/src/main/kotlin/Main2Test.kt
@@ -1,6 +1,26 @@
 package scraping
 
-fun main() {
+import it.skrape.fetcher.request.Json
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okio.IOException
+import com.google.gson.GsonBuilder
 
+fun main() {
+    val client = OkHttpClient()
+
+    val request = Request.Builder()
+        .url("http://localhost:3001/restaurants")
+        .build()
+
+    client.newCall(request).execute().use { response ->
+        if (!response.isSuccessful) throw IOException("Unexpected code $response")
+
+        for ((name, value) in response.headers) {
+            println("TESTING: $name: $value\n")
+        }
+
+        println(response.body!!.string())
+    }
 }
 
