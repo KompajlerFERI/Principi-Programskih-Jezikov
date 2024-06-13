4215c84c716493cdbc4f8981273cda911a758c5f
Sluzek <jan.valiser@gmail.com>
2024-06-13 20:06:25 +0200

    [DD-270] Add removeUser to RemoveFromDatabase

--- DD_PPJ_NALOGA02/Database_Interface/src/main/kotlin/util/RemoveFromDatabase.kt
+++ DD_PPJ_NALOGA02/Database_Interface/src/main/kotlin/util/RemoveFromDatabase.kt
@@ -2,11 +2,29 @@ package util
 
 import okhttp3.OkHttpClient
 import okhttp3.Request
-import scraper.Menu
-import scraper.Restaurant
-import scraper.RestaurantList
+import scraper.*
 
 object RemoveFromDatabase {
+    fun removeUser(user: User) {
+        Auth.login("Sluzek", "1234")
+
+        val client = OkHttpClient()
+
+        val request = Request.Builder()
+            .url("http://localhost:3001/users/${user.id}")
+            .delete()
+            .addHeader("Cookie", Auth.cookie)
+            .build()
+
+        client.newCall(request).execute().use { response ->
+            if (!response.isSuccessful) throw Exception("Unexpected code $response")
+
+            println("Response body: " + response.body!!.string())
+        }
+        if (UserList.users.contains(user)) {
+            UserList.users.remove(user)
+        }
+    }
     fun removeRestaurant(restaurant: Restaurant) {
         if (!restaurant.isInDatabase) {
             println("Restaurant is not in the database.")
